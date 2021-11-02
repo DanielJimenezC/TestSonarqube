8505f908e2fe54bbbf59b967e6db703deff40108
Daniel Jimenez <danieljimenezcanales@gmail.com>
2021-11-02 11:58:54 -0500

    new

--- Command.cmd
+++ Command.cmd
@@ -1,3 +1,3 @@
-dotnet sonarscanner begin /k:"test" /d:sonar.host.url="http://localhost:9000"  /d:sonar.login="4cd3768033086152d04ae856256a20727fd608e9"
-dotnet build
-dotnet sonarscanner end /d:sonar.login="4cd3768033086152d04ae856256a20727fd608e9"
+cd %system.agent.home.dir%
+C:\sonarqube\bin\MSBuild.SonarQube.Runner.exe begin /k:"test" /d:sonar.host.url="http://localhost:9000"  /d:sonar.login="4cd3768033086152d04ae856256a20727fd608e9"
+C:\sonarqube\bin\MSBuild.SonarQube.Runner.exe end /d:sonar.login="4cd3768033086152d04ae856256a20727fd608e9"
