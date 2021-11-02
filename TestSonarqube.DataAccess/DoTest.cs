c086a836b49023cefc484740386fdb1e875b6e1f
Daniel Jimenez <danieljimenezcanales@gmail.com>
2021-11-02 14:31:48 -0500

    correct coverage

--- TestSonarqube.DataAccess/DoTest.cs
+++ TestSonarqube.DataAccess/DoTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using TestSonarqube.Domain.Dto;
 
 namespace TestSonarqube.DataAccess
 {
+    [ExcludeFromCodeCoverage]
     public class DoTest
     {
         private List<TestModal> testModals;
